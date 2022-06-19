8eeff0fb3bf946d1aa93384d1d572f4ad7738ccb
Andrew Basson <andrew.basson@gmail.com>
2022-06-19 18:25:39 +0200

    fix(pa): install to sane locations
    Signed-off-by: Andrew Basson <andrew.basson@gmail.com>

--- pa-install.sh
+++ pa-install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
 cd ~/dev
-git clone https://github.com/ksanislo/klipper-pa_cal.git
+git clone https://github.com/ksanislo/klipper-pa_cal.git pa_cal
 cd klipper-pa_cal
 ./install-pa_cal.sh
+
+MR_CONFIG=~/klipper_config/moonraker.conf
+sed -i -E 's/klipper-pa_cal/\/dev/pa_cal/g' "${MR_CONFIG}"
+sed -i -E '/is_system_service/d' "${MR_CONFIG}"
+sudo systemctl restart moonraker
